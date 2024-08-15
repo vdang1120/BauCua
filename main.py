9f423f9b8e9863cbf8b68ee83666feb4d4b21062
vdang1120 <vdang1120@gmail.com>
2024-08-15 00:07:29 -0500

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+from waitress import serve
 import random
 
 app = Flask(__name__)
@@ -42,5 +43,5 @@ def calculate_win(bet_amounts, dice_results):
 
     return win_amount
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
